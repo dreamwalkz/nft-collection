47abd3845ec6a40c58399bb20bdcda22eb57aa3d
Shawn <dreamfunds@gmail.com>
2022-10-25 16:18:32 +0800

    nft-coll commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x00f3c24cfec94da31335438a24eb55f5435285ca";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://whitelist-dappp-7igsurobb-dreamwalkz.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
